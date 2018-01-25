359e6978fbd698a13c17b46e44bdbae107fa7f3f
Javier G. Sogo <jgsogo@gmail.com>
2018-01-25 12:34:13 +0100

    adapt originl CMakeLists.txt

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,14 @@
 
 from conans import ConanFile, CMake, tools
 import os
+import shutil
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
+class SPDLogSetupConan(ConanFile):
+    name = "spdlog_setup"
+    version = "master"
+    url = "https://github.com/bincrafters/conan-spdlog_setup"
+    description = "spdlog setup initialization via file configuration for convenience."
 
     # Indicates License type of the packaged library
     license = "MIT"
@@ -23,8 +24,8 @@ class LibnameConan(ConanFile):
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"build_tests": [True, False]}
+    default_options = "build_tests=True"
 
     # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
@@ -32,14 +33,44 @@ class LibnameConan(ConanFile):
 
     # Use version ranges for dependencies unless there's a reason not to
     requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/[>=1.2.11]@conan/stable"
+        "cpptoml/master@jgsogo/testing",
+        "rustfp/master@jgsogo/testing",
+        "fmt/[>=4.1.0]@bincrafters/stable",
+        "spdlog/[>=0.14.0]@bincrafters/stable",
+        "catch2/[>=2.1.0]@bincrafters/stable"
     )
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        source_url = "https://github.com/guangie88/spdlog_setup"
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
+
+        # Work to remove 'deps' directory (conan will handle them)
+        shutil.rmtree(os.path.join(extracted_dir, "deps"))
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"), "add_subdirectory(deps/cpptoml)", "")
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"), "add_subdirectory(deps/rustfp)", "")
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"), "add_subdirectory(deps/spdlog)", "")
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"), "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/deps/fmt>", "")
+
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"),
+'''
+install(DIRECTORY deps/fmt/fmt DESTINATION include
+  PATTERN deps/fmt/fmt/*.txt EXCLUDE)
+''', "")
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"),
+'''
+  target_include_directories(spdlog_setup_unit_test
+    PRIVATE
+      $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/deps/Catch/include>)
+''', "")
+        tools.replace_in_file(os.path.join(extracted_dir, "CMakeLists.txt"),
+'''
+target_link_libraries(spdlog_setup
+  INTERFACE
+    cpptoml
+    rustfp
+    spdlog)
+''', "")
 
         #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
@@ -47,23 +78,17 @@ class LibnameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
+        cmake.definitions["SPDLOG_SETUP_INCLUDE_UNIT_TESTS"] = self.options.build_tests
         cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
+        if self.options.build_tests:
+            # cmake.test()
+            pass
         cmake.install()
 
     def package(self):
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
         self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+    
+    def package_id(self):
+        self.info.header_only()
 
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
